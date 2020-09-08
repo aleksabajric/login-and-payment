16a143d340125d1fdfca84023652776b128c69d5
Aleksa Bajric <36825811+aleksabajric@users.noreply.github.com>
2020-09-08 20:45:37 +0200

    big clean up and refactor

--- security/src/main/java/com/security/SecurityApplication.java
+++ security/src/main/java/com/security/SecurityApplication.java
@@ -2,9 +2,17 @@ package com.security;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SecurityApplication {
+
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(SecurityApplication.class, args);
--- security/src/main/java/com/security/ctrl/LoginCtrl.java
+++ security/src/main/java/com/security/ctrl/LoginCtrl.java
@@ -1,16 +1,22 @@
 package com.security.ctrl;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.Collections;
+import java.util.Map;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @RestController
-@RequestMapping(value = "/login")
 public class LoginCtrl {
+    private static final Logger logger = LoggerFactory.getLogger(LoginCtrl.class);
 
-    @GetMapping
-    private String hello(){
-        return "Hello!";
+    @GetMapping("/user")
+    public Map<String, Object> user(@AuthenticationPrincipal OAuth2User principal) {
+        logger.info(String.format("User :" + principal.getName()));
+        return Collections.singletonMap("name", principal.getAttribute("name"));
     }
 
 }
